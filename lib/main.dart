18c7e229c7c68a33d4a5c74ad788e31f61b03e21
Bidhan Marasine <bidhanmarasine88@gmail.com>
2023-04-17 20:53:15 +0545

    added favourites and used map

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(Myapp());
@@ -6,19 +7,6 @@ void main() {
 
 class Myapp extends StatelessWidget {
   Myapp({super.key});
-
-  List<String> task1 = ["Gym", "College", "Home", "Work"];
-  List<String> tast2 = [
-    "Leg day",
-    "Presentation on list view",
-    "Grab breakfast",
-    "Go to work"
-  ];
-  List<String> date1 = [
-    "1",
-    "2",
-    "4",
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,56 +16,90 @@ class Myapp extends StatelessWidget {
         appBar: AppBar(title: Center(child: Text('To Do List'))),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: tiles(task1: task1, date1: date1, tast2: tast2),
+          child: Tiles(),
         ),
       ),
     );
   }
 }
 
-class tiles extends StatelessWidget {
-  const tiles({
+class Tiles extends StatelessWidget {
+  Tiles({
     super.key,
-    required this.task1,
-    required this.date1,
-    required this.tast2,
   });
-
-  final List<String> task1;
-  final List<String> date1;
-  final List<String> tast2;
+  bool isFav = true;
+  RxList<RxMap<String, dynamic>> tasks = RxList([
+    RxMap({
+      "title": "Gym",
+      "description": "go to the gym",
+      "date": "1",
+      "icon": Icons.favorite
+    }),
+    RxMap({
+      "title": "School day",
+      "description": "go to the gym",
+      "date": "2",
+      "icon": Icons.favorite
+    }),
+    RxMap({
+      "title": "Work day",
+      "description": "go to the gym",
+      "date": "3",
+      "icon": Icons.favorite
+    }),
+  ]);
 
   @override
   Widget build(BuildContext context) {
+    var isFav = true.obs;
     return ListView.builder(
         itemExtent: 100,
-        itemCount: task1.length,
+        itemCount: tasks.length,
         itemBuilder: (context, index) {
           return Container(
-            margin: EdgeInsets.only(top: 10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                  width: 0.5, color: Color.fromARGB(255, 104, 102, 102)),
-            ),
-            child: ListTile(
-              minVerticalPadding: 20,
-              leading: CircleAvatar(
-                  backgroundColor: Color.fromARGB(255, 175, 206, 232),
-                  child: Text(date1[index])),
-              title: Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: Text(
-                  task1[index],
-                  style: TextStyle(fontSize: 25),
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    width: 0.5, color: Color.fromARGB(255, 104, 102, 102)),
+              ),
+              child: ListTile(
+                minVerticalPadding: 20,
+                leading: CircleAvatar(
+                    backgroundColor: Color.fromARGB(255, 175, 206, 232),
+                    child: Text(
+                      tasks[index]["date"],
+                    )),
+                title: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Text(
+                    tasks[index]["title"],
+                    style: TextStyle(fontSize: 25),
+                  ),
                 ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(left: 8.0, top: 8),
-                child: Text(tast2[index]),
-              ),
-              trailing: Icon(Icons.favorite),
-            ),
-          );
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(left: 8.0, top: 8),
+                  child: Text(
+                    tasks[index]["description"],
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {
+                    // isFav.value
+                    //     ? tasks[index]["icon"] = Icons.favorite
+                    //     : tasks[index]["icon"] = Icons.favorite;
+                    // print(isFav.value);
+                    isFav.value = !isFav.value;
+                    tasks.value[index].update(
+                        "icon",
+                        (value) => isFav.value
+                            ? Icons.favorite
+                            : Icons.favorite_border);
+                    print(tasks);
+                  },
+                  icon: Obx(() => Icon(tasks[index]["icon"])),
+                ),
+              ));
         });
   }
 }
